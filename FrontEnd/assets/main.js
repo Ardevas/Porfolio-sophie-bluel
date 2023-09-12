d20bc656c1fa943fe645cde4c5b4d4461f0d5c31
tho.bardot@gmail.com <tho.bardot@gmail.com>
2023-09-12 10:03:00 +0200

    Add categories selection

--- FrontEnd/assets/main.js
+++ FrontEnd/assets/main.js
@@ -1,21 +1,71 @@
-
 const gallery = document.querySelector(".gallery");
+const menu = document.getElementById("menu");
+const galleryCategories = menu.querySelectorAll("li[id]");
 
+let worksData = [];
+let categoriesId = [];
+let selectedCategoryId = "0";
+
+// Get categories data
+function fetchCategories() {
+  fetch("http://localhost:5678/api/categories")
+    .then((response) => response.json())
+    .then((categories) => {
+      categoriesId = categories;
+      categoriesEventListeners();
+    });
+}
+
+// Get works data
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
   .then((works) => {
+  worksData = works;
 
-    for (let i = 0; i < works.length; i++) {
-        const figure = document.createElement("figure");
-        const galleryImage = document.createElement("img");
-        galleryImage.src = works[i].imageUrl;
-
-        const galleryLegend = document.createElement("figcaption");
-        galleryLegend.innerText = works[i].title;
-
-        figure.appendChild(galleryImage);
-        figure.appendChild(galleryLegend);
-
-        gallery.appendChild(figure);
-    }
+  // Display all images
+  selectedCategoryId = "0" ;
+  displayImagesByCategory();
 });
+
+// Display images for selected category
+function displayImagesByCategory() {
+  gallery.innerHTML = "";
+
+  worksData.forEach((work) => {
+    if (selectedCategoryId === "0" || work.categoryId === selectedCategoryId) {
+      const figure = document.createElement("figure");
+
+      const galleryImage = document.createElement("img");
+      galleryImage.src = work.imageUrl;
+
+      const galleryLegend = document.createElement("figcaption");
+      galleryLegend.innerText = work.title;
+
+      gallery.appendChild(figure);
+      figure.appendChild(galleryImage);
+      figure.appendChild(galleryLegend);
+    }
+  });
+}
+
+// Categories event listener function
+function categoriesEventListeners() {
+  galleryCategories.forEach((li) => {
+    li.addEventListener("click", function () {
+      const clickedCategoryId = li.id;
+  
+      if (clickedCategoryId === "0") {
+        selectedCategoryId = "0";
+      } else {
+        const matchedCategory = categoriesId.find((category) => category.id.toString() === clickedCategoryId);
+        if (matchedCategory) {
+          selectedCategoryId = matchedCategory.id;
+        }
+      }
+  
+      displayImagesByCategory();
+    });
+  });
+}
+
+fetchCategories();
